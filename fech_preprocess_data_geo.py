c87df858ebc7d408345cc19a41a5f8d682489485
francisvalguedes <francisval.guedes.soares.037@ufrn.edu.br>
2023-08-20 14:30:24 -0300

    commit

--- fech_preprocess_data_geo.py
+++ fech_preprocess_data_geo.py
@@ -364,9 +364,9 @@ truncar_traj = True
 plot = True
 
 # reamostragem dos dados enable_resample:
-    # 0-não faz reamostragem, 
-    # 1-reamostragem com passo de tempo fixo (sample_new) com pandas.ffil
-    # 2-reamostragem quantidade de amostras fixa (sample_step), TR fica quebrado
+    # 0- não faz reamostragem (recomendado quando se deseja a mesma amostragem do bruto (.d)) para o arquivo de saída, 
+    # 1- reamostragem com passo de tempo fixo (sample_new) com pandas.ffil (recomendado quando se deseja um tempo de amostragem maior do que o bruto (.d) para o arquivo de saída)
+    # 2-reamostragem quantidade de amostras fixa (sample_step), TR fica quebrado (similar ao sistema antigo)
 enable_resample = 1
 # se enable_resample = 1 ajusta:
 sample_new = 1  # novo tempo de amostragem em segundos 
